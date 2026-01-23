ebc9951c7f4d9c96dcb7e0e67c1c60405afeed72
EvangMM <matteomartini92@yahoo.it>
2026-01-23 14:41:19 +0100

    fix: dag

--- s4-mlflow/src/pipeline.py
+++ s4-mlflow/src/pipeline.py
@@ -1,10 +1,10 @@
 from digitalhub_runtime_hera.dsl import step
-from hera.workflows import Steps, Workflow
+from hera.workflows import DAG, Workflow
 
 
 def pipeline():
     with Workflow(entrypoint="dag") as w:
-        with Steps(name="dag"):
+        with DAG(name="dag"):
             A = step(
                 template={"action": "job"},
                 function="train-mlflow-model",
